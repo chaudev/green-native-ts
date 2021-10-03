346f3df8669be42e9593300caa940fd1bcdf387e
chaudev <soc900k@mail.com>
2021-10-04 06:51:26 +0700

    Update Styles

--- src/styles/greenStyles/index.tsx
+++ src/styles/greenStyles/index.tsx
@@ -2,18 +2,45 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 
 export const GreenStyles = StyleSheet.create({
-  Center: {
+  center: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  Row: {
+  row: {
+    flexDirection: 'row',
+  },
+  centerRow: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-  ColumCenter: {
+  spaceBetweenRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  spaceAroundRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  centerColumn: {
+    flexDirection: 'column',
     alignItems: 'center',
   },
-  ShadowBox: {
+  spaceBetweenColumn: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  spaceAroundColumn: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  boxShadow: {
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -23,8 +50,125 @@ export const GreenStyles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 3,
   },
-  Title: {
+  title: {
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  text6: {
+    fontSize: 6,
+  },
+  text8: {
+    fontSize: 8,
+  },
+  text10: {
+    fontSize: 10,
+  },
+  text12: {
+    fontSize: 12,
+  },
+  text14: {
+    fontSize: 14,
+  },
+  text16: {
+    fontSize: 16,
+  },
+  text18: {
+    fontSize: 18,
+  },
+  text20: {
+    fontSize: 20,
+  },
+  text22: {
+    fontSize: 22,
+  },
+  text24: {
+    fontSize: 24,
+  },
+  text26: {
+    fontSize: 26,
+  },
+  text28: {
+    fontSize: 28,
+  },
+  text30: {
+    fontSize: 30,
+  },
+  text32: {
+    fontSize: 32,
+  },
+  text34: {
+    fontSize: 34,
+  },
+  text36: {
+    fontSize: 36,
+  },
+  text38: {
+    fontSize: 38,
+  },
+  text40: {
+    fontSize: 40,
+  },
+  text42: {
+    fontSize: 42,
+  },
+  text44: {
+    fontSize: 44,
+  },
+  text46: {
+    fontSize: 46,
+  },
+  text48: {
+    fontSize: 48,
+  },
+  text50: {
+    fontSize: 50,
+  },
+  textBold: {
+    fontWeight: 'bold',
+  },
+  textNormal: {
+    fontWeight: 'normal',
+  },
+  textUppercase: {
+    textTransform: 'uppercase',
+  },
+  textLowercase: {
+    textTransform: 'lowercase',
+  },
+  textNone: {
+    textTransform: 'none',
+  },
+  textRed: {
+    color: 'red',
+  },
+  textBlack: {
+    color: 'black',
+  },
+  textWhite: {
+    color: '#fff',
+  },
+  textGreen: {
+    color: 'green',
+  },
+  textBlue: {
+    color: 'blue',
+  },
+  textPink: {
+    color: 'pink',
+  },
+  backgroundRed: {
+    backgroundColor: 'red',
+  },
+  backgroundGreen: {
+    backgroundColor: 'green',
+  },
+  backgroundBlue: {
+    backgroundColor: 'blue',
+  },
+  backgroundBlack: {
+    backgroundColor: 'black',
+  },
+  backgroundWhite: {
+    backgroundColor: 'white',
   },
 });
